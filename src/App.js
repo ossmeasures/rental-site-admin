2dbff4bfb759fa158a3263ed12236065080488dc
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2021-02-20 16:41:28 +0900

    react-routerを導入して画面遷移できるようにする

--- src/App.js
+++ src/App.js
@@ -1,212 +1,112 @@
-import 'rsuite/lib/styles/index.less';
-import { Button, FlexboxGrid } from 'rsuite';
-import './App.css';
-import { List } from 'rsuite';
+import "rsuite/lib/styles/index.less";
+import {
+  Nav,
+  Navbar,
+  Dropdown,
+  Icon,
+  Container,
+  Header,
+  Content,
+  Footer,
+} from "rsuite";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import React from "react";
+import { RentalItems, Home, Users } from "./pages";
+import { useWindowDimensions } from "./useWindowDimentions";
+import { Logo } from "./driller.icon";
 
-const items = [
-    {
-        id: "1",
-        name: "2tダンプ",
-        // image: require("../assets/0f62134bf3e8ef07bc0f4f43076a3204.jpg"),
-        category: "トラック",
-        price: 150000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description:
-            "準中型ＭＴ免許ＯＫ　２トン２ｔベース１．５トン１．５ｔ積載　パネルバン左扉ありリア観音扉　全低床　ディーゼルＮＯＸｐｍ法適合車　車検時記録簿１６枚あり　全高２７３ｃｍトラック　３人乗り　ＭＴ５速",
-        orderStatus: "レンタル中",
-        rentalTermTo: "2020/02/26",
-        rentalTermFrom: "2020/02/18"
-    },
-    {
-        id: "2",
-        name: "2tトラッククレーン",
-        // image: require("../assets/1afd850e68d4297b622ab28d01a4b2ac.jpg"),
-        category: "クレーン",
-        price: 250000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "注文処理中",
-        rentalTermTo: "2020/02/09",
-        rentalTermFrom: "2020/02/01"
-    },
-    {
-        id: "3",
-        name: "ワンボックス",
-        // image: require("../assets/2b45c503eec0ec1bd12c27d911b2e230.jpg"),
-        category: "バン",
-        price: 8000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "注文処理中",
-        rentalTermTo: "2020/01/18",
-        rentalTermFrom: "2020/01/02"
-    },
-    {
-        id: "4",
-        name: "油圧ショベルA型",
-        // image: require("../assets/8ce94c3bf2b79dbf5c771da11e08941f.jpg"),
-        category: "油圧ショベル",
-        price: 120000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "注文処理中",
-        rentalTermTo: "2020/01/29",
-        rentalTermFrom: "2020/01/10"
-    },
-    {
-        id: "5",
-        name: "カニクレーン",
-        // image: require("../assets/22b080faae9bb2bf646ee92ef42b2b4c.jpg"),
-        category: "クレーン",
-        price: 105000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "レンタル中",
-        rentalTermTo: "2020/02/18",
-        rentalTermFrom: "2020/01/25"
-    },
-    {
-        id: "6",
-        name: "軽トラック",
-        // image: require("../assets/75fca98d5ab45ffc1e5d02f4e25435c0.jpg"),
-        category: "トラック",
-        price: 5000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "注文処理中",
-        rentalTermTo: "2020/02/18",
-        rentalTermFrom: "2020/02/25"
-    },
-    {
-        id: "7",
-        name: "トラック d",
-        // image: require("../assets/77cf4f40da07636d792d5b9246648232.jpg"),
-        category: "トラック",
-        price: 2000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "注文処理中",
-        rentalTermTo: "2020/02/18",
-        rentalTermFrom: "2020/02/25"
-    },
-    {
-        id: "8",
-        name: "トラック e",
-        // image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
-        category: "トラック",
-        price: 4000,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "レンタル中",
-        rentalTermTo: "2020/02/18",
-        rentalTermFrom: "2020/02/25"
-    },
-    {
-        id: "9",
-        name: "トラック f",
-        // image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
-        category: "トラック",
-        price: 7500,
-        modelYear: "2006",
-        mileage: 450000,
-        displacement: "4900cc",
-        hasRepairHistory: true,
-        hasWarranty: false,
-        mission: "5速MT",
-        description: "説明文",
-        orderStatus: "未レンタル",
-        rentalTermTo: "2020/02/18",
-        rentalTermFrom: "2020/02/25"
-    },
-];
-
-const styleCenter = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '60px'
-};
-
-const styleColor = (orderStatus) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '60px',
-    color: orderStatus === "レンタル中" || orderStatus === "配送中" ? 'orange' : 'gray'
+const NavLink = React.forwardRef((props, ref) => {
+  const location = useLocation();
+  return (
+    <Nav.Item
+      {...props}
+      ref={ref}
+      active={props.to === location.pathname}
+      componentClass={Link}
+    />
+  );
 });
 
-function App() {
+const navBarBrandLogo = {
+  padding: "5px 5px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "18px",
+  fontWeight: "bold",
+};
 
-    return (
-        <div className="App">
-            <List>
-                {items.map((item, index) => (
-                    <List.Item key={index} index={index}>
-                        <FlexboxGrid>
-                            <FlexboxGrid.Item colspan={6} style={styleCenter}>
-                                {item['name']}
-                            </FlexboxGrid.Item>
-                            <FlexboxGrid.Item colspan={3} style={styleCenter}>
-                                {item['category']}
-                            </FlexboxGrid.Item>
-                            <FlexboxGrid.Item colspan={3} style={styleColor(item['orderStatus'])}>
-                                {item['orderStatus']}
-                            </FlexboxGrid.Item>
-                            <FlexboxGrid.Item colspan={4} style={styleCenter}>
-                                {item['rentalTermFrom']}
-                            </FlexboxGrid.Item>
-                            <FlexboxGrid.Item colspan={4} style={styleCenter}>
-                                {item['rentalTermTo']}
-                            </FlexboxGrid.Item>
-                            <FlexboxGrid.Item colspan={3} style={styleCenter}>
-                                <Button color="orange" appearance="ghost"> 詳細 </Button>
-                            </FlexboxGrid.Item>
-                        </FlexboxGrid>
-                    </List.Item>
-                ))}
-            </List>
-        </div >
-    );
+const NavBarInstance = ({ ...props }) => {
+  return (
+    <Navbar {...props} appearance="inverse">
+      <Navbar.Header style={navBarBrandLogo}>
+        <Logo
+          alt="logo"
+          style={{ height: "70%", width: "auto", fill: "white" }}
+        />
+        れんたるさいと
+      </Navbar.Header>
+      <Navbar.Body>
+        <Nav>
+          <NavLink to="/" icon={<Icon icon="home" />}>
+            Home
+          </NavLink>
+          <NavLink to="/rental/items">レンタル商品一覧</NavLink>
+          <NavLink to="/users">ユーザ一覧</NavLink>
+          <Dropdown title="About">
+            <Dropdown.Item eventKey="4">Company</Dropdown.Item>
+            <Dropdown.Item eventKey="5">Team</Dropdown.Item>
+            <Dropdown.Item eventKey="6">Contact</Dropdown.Item>
+          </Dropdown>
+        </Nav>
+        <Nav pullRight>
+          <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+        </Nav>
+      </Navbar.Body>
+    </Navbar>
+  );
+};
+
+function App() {
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <Container>
+      <Router>
+        <Header>
+          <NavBarInstance />
+        </Header>
+        <Content
+          style={{
+            height: height * 0.95,
+            width: width * 0.9,
+            marginTop: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Switch>
+            <Route path="/rental/items">
+              <RentalItems />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Content>
+        <Footer>Footer</Footer>
+      </Router>
+    </Container>
+  );
 }
 
 export default App;
