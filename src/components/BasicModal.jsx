52475cdf7b4f52774de7cfe102e70a82d90926e6
hasegawa-abcde <hasegawa_shun@intec.co.jp>
2021-03-05 04:26:49 +0000

    モーダルレイアウト修正

--- src/components/BasicModal.jsx
+++ src/components/BasicModal.jsx
@@ -1,13 +1,33 @@
-import { Modal, Button, ButtonToolbar } from 'rsuite';
-import React from 'react';
+import { Modal, Button, ButtonToolbar } from "rsuite";
+import React from "react";
 
-
+const items = [
+    {
+        id: "ID",
+        name: "名前",
+        image: "画像",
+        category: "カテゴリ",
+        price: "価格",
+        modelYear: "製造日",
+        mileage: "走行距離",
+        displacement: "名前",
+        hasRepairHistory: "名前",
+        hasWarranty: "名前",
+        mission: "名前",
+        description: "名前",
+        orderStatus: "名前",
+        rentalTermTo: "名前",
+        rentalTermFrom: "名前",
+        deliveryAddress: "名前",
+        // user: { name: "長谷川", email: "test@com" },
+    },
+]
 
 class BasicModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: false
+            show: false,
         };
         this.close = this.close.bind(this);
         this.open = this.open.bind(this);
@@ -30,17 +50,33 @@ class BasicModal extends React.Component {
                         <Modal.Title>{this.props.item.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <div><th>名前</th>{this.props.item.name}</div>
-                        <div>{this.props.item.category}</div>
-                        <div>{this.props.item.price}</div>
-                        <div>{this.props.item.modelYear}</div>
-                        <div>{this.props.item.mileage}</div>
-                        <div>{this.props.item.displacement}</div>
-                        <div>{this.props.item.mission}</div>
-                        <div>{this.props.item.description}</div>
-                        <div>{this.props.item.orderStatus}</div>
-                        <div>{this.props.item.rentalTermTo}</div>
-                        <div>{this.props.item.rentalTermFrom}</div>
+                        <div>
+                            <img
+                                src={this.props.item.image}
+                                style={{ width: 240, height: 240, borderRadius: 10 }}
+                            ></img>
+                        </div>
+                        <div style={{ display: "flex" }}>
+                            <div style={{ width: "100px", paddingRight: "10px" }}>カテゴリ</div>
+                            <div>{this.props.item.category}</div>
+                        </div>
+                        <div style={{ display: "flex" }}>
+                            <div style={{ width: "100px", paddingRight: "10px" }}>価格</div>
+                            <div>{this.props.item.price}</div>
+                        </div>
+                        <div>
+                            {Object.keys(this.props.item).map(key => <div style={{ display: "flex" }}>
+                                <div style={{ width: "100px", paddingRight: "10px" }}>{key}</div>
+                                <div>{this.props.item[key]}</div>
+                            </div>)}
+                            {/* <List bordered>
+                                {data.map((item, index) => (
+                                    <List.Item key={index} index={index}>
+                                        {item}
+                                    </List.Item>
+                                ))}
+                            </List> */}
+                        </div>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button onClick={this.close} appearance="primary">
